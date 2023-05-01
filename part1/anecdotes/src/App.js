d4839c5731696ac3096564c8751d1d04d36d20ec
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-05-01 01:29:25 +0000

    Completed 1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,16 +13,27 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
 
-  const handleClick = () => {
+  const handleNext = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
   }
 
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
 
-      <button onClick={handleClick}>
+      <button onClick={handleVote}>
+        vote
+      </button>
+      <button onClick={handleNext}>
         next anecdote
       </button>
     </div>
