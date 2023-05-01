8e6127536feed0a0652c86c9f8eb9d05d5fe3133
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-05-01 00:58:31 +0000

    Completed 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 
-const Stats = ({good, bad, neutral}) => {
+const StatisticsLine = ({text, value, unit}) => {
+    return <p>{text} {value}{unit}</p>
+}
+
+const Statistics = ({good, bad, neutral}) => {
     const all = good + bad + neutral
     if (all === 0) {
         return (
@@ -16,14 +20,18 @@ const Stats = ({good, bad, neutral}) => {
     return (
         <>
             <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive * 100}%</p>
+            <StatisticsLine text="good" value={good} />
+            <StatisticsLine text="neutral" value={neutral} />
+            <StatisticsLine text="bad" value={bad} />
+            <StatisticsLine text="all" value={all} />
+            <StatisticsLine text="average" value={average} />
+            <StatisticsLine text="positive" value={positive * 100} unit="%" />
         </>
     )
+}
+
+const Button = ({text, handleClick}) => {
+    return <button onClick={handleClick}>{text}</button>
 }
 
 const App = () => {
@@ -35,10 +43,10 @@ const App = () => {
     return (
         <div>
             <h1>give feedback</h1>
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
-            <Stats good={good} neutral={neutral} bad={bad} />
+            <Button text="good" handleClick={() => setGood(good + 1)} />
+            <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+            <Button text="bad" handleClick={() => setBad(bad + 1)} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
