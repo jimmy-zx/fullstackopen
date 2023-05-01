91a7585c5d3f5b5a6c5b5cc24389eef8d07556d9
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-05-01 22:42:52 +0000

    Completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,10 +5,23 @@ const Person = ({person}) =>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {id: 1, name: 'Arto Hellas', number: '040-1234567'},
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [displayPersons, setDisplayPersons] = useState(persons)
+
+  const updateFilter = (event) => {
+    setFilter(event.target.value)
+    setDisplayPersons(persons.filter(
+      (person) => person.name.toLowerCase().indexOf(event.target.value) !== -1
+    ))
+  }
 
   const handleAdd = (event) => {
     event.preventDefault()
@@ -22,13 +35,18 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={filter} onChange={updateFilter} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>name: <input value={newName} onChange={(event) => setNewName(event.target.value)} /></div>
         <div>number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} /></div>
         <div><button type="submit" onClick={handleAdd}>add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <Person key={person.name} person={person} />)}
+      {displayPersons.map((person) => <Person key={person.name} person={person} />)}
     </div>
   )
 }
