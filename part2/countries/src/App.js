2abb3b3af4ef20ddd24888e0ff96dfc1b9e2d37d
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-05-05 18:51:40 +0000

    Completed 2.19

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,47 +3,60 @@ import axios from 'axios';
 
 const countryAPI = "https://restcountries.com/v3.1/all";
 
+const CountryData = ({ name, show }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    if (show) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}`)
+        .then((resp) => setData(resp.data[0]));
+    } else {
+      setData(null);
+    }
+  }, [name, show]);
+
+  if (data) {
+    return (
+      <>
+        <h2>{name}</h2>
+        <p>capital {data.capital}</p>
+        <p>area {data.area}</p>
+        <h3>languages:</h3>
+        <ul>
+          {Object.keys(data.languages).map((key) => <li key={key}>{data.languages[key]}</li>)}
+        </ul>
+        <img src={data.flags.png} alt="flag" />
+      </>
+    );
+  }
+  return null;
+}
+
+const Country = ({ name }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div>
+      {name}
+      <button onClick={() => setShow(!show)}>show</button>
+      <CountryData name={name} show={show} />
+    </div>
+  )
+}
+
 const Filter = ({ countries, name }) => {
-  const [capital, setCapital] = useState('');
-  const [area, setArea] = useState(0);
-  const [languages, setLanguages] = useState([]);
-  const [flagUrl, setFlagUrl] = useState('');
   const dispCountry = countries.filter((country) =>
     country.name.common.toLowerCase().includes(name.toLowerCase())
   );
-
-  useEffect(() => {
-    if (dispCountry.length === 1) {
-      axios
-        .get(`https://restcountries.com/v3.1/name/${dispCountry[0].name.common}`)
-        .then((resp) => {
-          setCapital(resp.data[0].capital);
-          setArea(resp.data[0].area);
-          setLanguages(resp.data[0].languages);
-          setFlagUrl(resp.data[0].flags.png);
-        });
-    }
-  }, [dispCountry, name]);
 
   if (dispCountry.length > 10) {
     return (
       <>Too many matches, specify another filter</>
     );
   } else if (dispCountry.length === 1) {
-    return (
-      <>
-        <h2>{dispCountry[0].name.common}</h2>
-        <p>capital {capital}</p>
-        <p>area {area}</p>
-        <h3>languages:</h3>
-        <ul>
-          {Object.keys(languages).map((key) => <li key={key}>{languages[key]}</li>)}
-        </ul>
-        <img src={flagUrl} alt="flag" />
-      </>
-    );
+    return <CountryData name={dispCountry[0].name.common} show={true} />
   } else {
-    return dispCountry.map((country, index) => <p key={index}>{country.name.common}</p>);
+    return dispCountry.map((country, index) => <Country key={index} name={country.name.common} />)
   };
 };
 
