a0e93fba1570aac48ecba21cdcc0dd1b76144d84
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-05-05 19:35:47 +0000

    Completed 2.20

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,34 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const countryAPI = "https://restcountries.com/v3.1/all";
+const WeatherData = ({ name }) => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://geocoding-api.open-meteo.com/v1/search?name=${name}&count=1&language=en&format=json`)
+      .then((resp) => {
+        var lat = resp.data.results[0].latitude;
+        var lon = resp.data.results[0].longitude;
+        axios
+          .get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true&windspeed_unit=ms`)
+          .then((resp) => setData(resp.data));
+      });
+  }, [name])
+
+  if (data) {
+    var code = String(data.current_weather.weathercode).padStart(2, '0');
+    return (
+      <>
+        <h3>Weather in {name}</h3>
+        <p>temperature {data.current_weather.temperature} Celcius</p>
+        <img src={`https://www.meteopool.org/theme/images/wwCodes/${code}.gif`} alt={code} />
+        <p>wind {data.current_weather.windspeed} m/s</p>
+      </>
+    )
+  }
+  return null;
+}
 
 const CountryData = ({ name, show }) => {
   const [data, setData] = useState(null);
@@ -27,6 +54,7 @@ const CountryData = ({ name, show }) => {
           {Object.keys(data.languages).map((key) => <li key={key}>{data.languages[key]}</li>)}
         </ul>
         <img src={data.flags.png} alt="flag" />
+        <WeatherData name={data.capital} />
       </>
     );
   }
@@ -65,7 +93,7 @@ const App = () => {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-    axios.get(countryAPI).then((resp) => {
+    axios.get("https://restcountries.com/v3.1/all").then((resp) => {
       setCountries(resp.data);
     });
   }, []);
