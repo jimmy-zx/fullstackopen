c14c201f455addcb4bc7e6facb69ff60371ae823
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-08-02 20:56:19 +0000

    Completed 3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -50,19 +50,36 @@ app.get('/info', (request, response) => {
     `);
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (!person) {
-      return response.status(404).end();
-    }
-    response.json(person);
-  });
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end();
+      }
+      response.json(person);
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  data = data.filter(person => person.id !== id);
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
