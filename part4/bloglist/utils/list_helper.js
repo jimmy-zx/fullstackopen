c01bfcb42abccb1efe9ae0e2e4ce991b49f60906
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-08-11 18:56:31 +0000

    Completed 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((psum, blog) => psum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((fav, blog) => blog.likes > fav.likes ? blog : fav, blogs[0]);
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
