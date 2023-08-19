eec080edf8766f68f118bb6ebc614b7a23f54415
Jimmy Z <51149050+jimmy-zx@users.noreply.github.com>
2023-08-19 20:44:19 +0000

    Completed 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../utils/config');
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user');
@@ -8,10 +18,17 @@ blogsRouter.get('/', async (request, response) => {
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body);
+  const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  const blog = new Blog({...request.body,
+    user: user.id
+  });
   const savedBlog = await blog.save();
 
-  const user = await User.findById(request.body.user);
   user.blogs = user.blogs.concat(savedBlog._id);
 
   response.status(201).json(savedBlog);
---
+++ part4/bloglist/scripts/post.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TARGET=http://127.0.0.1:3003
+
+HEADERS=(-H Content-Type:Application/JSON)
+
+
+TOKEN=$(curl "${HEADERS[@]}" $TARGET/api/login --data '{"username": "root", "password": "sekret"}' | jq -r '.token')
+
+HEADERS+=(-H "Authorization:Bearer $TOKEN")
+
+curl "${HEADERS[@]}" $TARGET/api/blogs --data '{"title": "Title", "url": "example.com"}'
